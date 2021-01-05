9ae44cb1ad2dac4a1d25949c039dc0775f14f62c
tiago.missao <tiago.missao@kroton.com.br>
2021-01-05 08:23:02 -0300

    Outputs

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "s3_id" {
+  description = "S3`s ID"
+  value       = aws_s3_bucket.s3_bucket.id
+}
+
+output "s3_arn" {
+  description = "S3`s ARN"
+  value       =  aws_s3_bucket.s3_bucket.arn
+}
+
+output "dynamodb_id" {
+  description = "DynamoDB`s ID"
+  value       = aws_dynamodb_table.dynamodb.id
+}
+
+output "dynamodb_arn" {
+  description = "DynamoDB`s ARN"
+  value       =  aws_dynamodb_table.dynamodb.arn
+}
